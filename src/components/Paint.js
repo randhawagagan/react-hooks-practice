539e503bf09ac32266eb05fac87ee6986b87f0f2
Gagan <randhawa_gagan@live.com>
2019-12-20 19:26:43 +0530

    FInal commit

--- src/components/Paint.js
+++ src/components/Paint.js
@@ -1,11 +1,11 @@
-import React, { useState, useEffect, useRef, useCallback } from 'react';
-import Name from './Name';
-import styles from '../../style.css';
-import ColorPicker from './ColorPicker'
-import randomColor from 'randomcolor'
-import WindowSize from './WindowSize'
+import React, { useState, useEffect, useCallback, useRef } from 'react'
+import Name from './Name'
 import Canvas from './Canvas'
+import ColorPicker from './ColorPicker'
 import RefreshButton from './RefreshButton'
+import useWindowSize from './WindowSize'
+import randomColor from 'randomcolor'
+
 export default function Paint() {
   const [colors, setColors] = useState([])
   const [activeColor, setActiveColor] = useState(null)
@@ -19,6 +19,14 @@ export default function Paint() {
       })
   }, [])
   useEffect(getColors, [])
+
+  const [visible, setVisible] = useState(false)
+  let timeoutId = useRef()
+  const [windowWidth, windowHeight] = useWindowSize(() => {
+    setVisible(true)
+    clearTimeout(timeoutId.current)
+    timeoutId.current = setTimeout(() => setVisible(false), 500)
+  })
 
   return (
     <div className="app">
@@ -41,7 +49,9 @@ export default function Paint() {
           height={window.innerHeight}
         />
       )}
-      <WindowSize />
+      <div className={`window-size ${visible ? '' : 'hidden'}`}>
+        {windowWidth} x {windowHeight}
+      </div>
     </div>
   )
 }
